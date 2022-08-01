90467dac9336856cbd92cc855be84885d1b132dd
Danila Voronkov <danilavoronkov2002@gmail.com>
2022-08-01 20:24:31 +0300

    feat(env): Updatte api url

--- src/uttils/fetchApi.ts
+++ src/uttils/fetchApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string"
 
 export const getCars = async (key: any) => {
-  const apiUrl = process.env.API_URL || "http://localhost:3000"
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000"
   const parse = queryString.stringifyUrl({url: `${apiUrl}/api/cars`, query: key.queryKey[1]})
   const res = await axios.get(parse)
 
